9075618a9ece49393c10154bf9d9cd9f4ba97828
maxime-mn <maxime.mion@gmail.com>
2020-09-27 16:37:06 +0200

    feat: add command for last post

--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@ require('dotenv').config();
 
 const utils = require('./utils.js')
 const Discord = require('discord.js');
-
 const bot = new Discord.Client();
 const TOKEN = process.env.DISCORD_TOKEN;
-if (!TOKEN) throw "Missing bot token !!";
-
 const POLLING_TIME =  5 * 60000; // => x * 60000 where x is the time in minutes
 
+if (!TOKEN) throw "Missing bot token !!";
+
 bot.on('ready', async () => {
-    setInterval(utils.getLastPost, POLLING_TIME, bot);
+    console.info('Bot is ready')
+    setInterval(utils.pollNewPost, POLLING_TIME, bot);
+});
+
+bot.on('message', async (message) =>{
+    if(message.author.bot) return;
+    switch (message.content) {
+        case ";lastpost":
+            await utils.sendFormattedLastPost(message);
+            break;
+        default:
+            break;
+    }
 });
 
 bot.login(TOKEN);
--- src/utils.js
+++ src/utils.js
@@ -5,37 +5,49 @@ const URL = "https://hytale.com/api/blog/post/published";
 const LAST_FILE = "../config/last.json";
 
 module.exports = {
-    getLastPost: async function (bot) {
-
-        const lastElement = (await axios.get(URL)).data[0];
+    pollNewPost: async function (bot) {
+        const lastElement = await getLastPost();
         const { last } = lastSaved();
 
         if (lastElement._id !== last) {
             bot.guilds.cache.forEach(server => {
                 server.channels.cache.forEach(channel => {
                     if (channel.name === "hytale-news") {
-                        bot.channels.cache.get(channel.id).send({
-                            embed: {
-                                color: 3447003,
-                                author: {
-                                    name: 'New Hytale news !',
-                                },
-                                title: lastElement.title,
-                                url: `https://hytale.com/news/${new Date(lastElement.publishedAt).getFullYear()}/${new Date(lastElement.publishedAt).getMonth() + 1}/${lastElement.slug}`,
-                                description: lastElement.bodyExcerpt,
-                                timestamp: new Date(),
-                                image: {
-                                    url: `https://cdn.hytale.com/variants/blog_thumb_${lastElement.coverImage.s3Key}`,
-                                },
-                                footer: {
-                                    text: "© Hytale bot by maxime-mn"
-                                }
-                            }
-                        });
+                        bot.channels.cache.get(channel.id).send(formatElement(lastElement));
                     }
                 });
             });
             saveLast(lastElement._id);
+        }
+    },
+    sendFormattedLastPost: async function(message){
+        const data = await getLastPost();
+        const formattedMessage = formatElement(data);
+        message.channel.send(formattedMessage);
+    }
+}
+
+async function getLastPost(){
+    return (await axios.get(URL)).data[0];
+}
+
+function formatElement(data){
+    return {
+        embed: {
+            color: 3447003,
+            author: {
+                name: 'New Hytale news !',
+            },
+            title: data.title,
+            url: `https://hytale.com/news/${new Date(data.publishedAt).getFullYear()}/${new Date(data.publishedAt).getMonth() + 1}/${data.slug}`,
+            description: data.bodyExcerpt,
+            timestamp: new Date(),
+            image: {
+                url: `https://cdn.hytale.com/variants/blog_thumb_${data.coverImage.s3Key}`,
+            },
+            footer: {
+                text: "© Hytale bot by maxime-mn"
+            }
         }
     }
 }
